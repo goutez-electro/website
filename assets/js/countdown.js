9f784b9e32cbcbc60db6cd5a870c1322b738112d
jpep-in <dev@jpep.in>
2025-04-25 19:27:59 +0200

    countdown

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,36 +1,65 @@
-// Set the date we're counting down to
-var countDownDateStart = new Date("Jun 8, 2025 14:00:00 GMT+02").getTime();
-var countDownDateEnd = new Date("Jun 8, 2025 20:00:00 GMT+02").getTime();
+// Liste des événements avec des dates de début et de fin
+var events = [
+  { start: new Date("Apr 26, 2025 19:24:00 GMT+02").getTime(), end: new Date("Jun 8, 2025 20:00:00 GMT+02").getTime() },
+
+  { start: new Date("Jun 8, 2025 14:00:00 GMT+02").getTime(), end: new Date("Jun 8, 2025 20:00:00 GMT+02").getTime() },
+  { start: new Date("Sep 14, 2025 14:00:00 GMT+02").getTime(), end: new Date("Sep 14, 2025 20:00:00 GMT+02").getTime() },
+  { start: new Date("Sep 27, 2025 17:00:00 GMT+02").getTime(), end: new Date("Sep 27, 2025 23:00:00 GMT+02").getTime() },
+];
 
 $(function() {
-    countDown();
+  countDown();
 });
 
 function countDown() {
   // Get today's date and time
-  var now = new Date().getTime();  
-  // Find the distance between now and the count down date
-  var distance = countDownDateStart - now;
-  //alert('totote');
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-  // Display the result in the element with id="demo"
-  var result = "Prochain évènement dans " + days + "j " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  var now = new Date().getTime();
+  var result = "";
+  var nextEvent = null;
 
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    if (countDownDateEnd - now > 0) {
-        result = "Le Goûtez Electronique a lieu en ce moment !";
-    } else {
-        result = "";
+  // Parcourir la liste des événements pour trouver le prochain événement
+  for (var i = 0; i < events.length; i++) {
+    var event = events[i];
+    if (now < event.end) {
+      nextEvent = event;
+      break;
     }
   }
+
+  if (nextEvent) {
+    var distance = nextEvent.start - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="countDown"
+    result = "Prochain évènement dans ";
+    if (days > 0) {
+      result += days + "j ";
+    }
+    if (hours > 0) {
+      result += hours + "h ";
+    }
+    if (minutes > 0) {
+      result += minutes + "m ";
+    }
+    result += seconds + "s";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      if (nextEvent.end - now > 0) {
+        result = "L'événement a lieu en ce moment !";
+      } else {
+        result = "";
+      }
+    }
+  } else {
+    result = "Aucun événement à venir.";
+  }
+
   document.getElementById("countDown").innerHTML = result;
 }
 
